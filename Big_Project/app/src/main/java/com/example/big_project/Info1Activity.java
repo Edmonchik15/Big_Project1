bf8d7ddcea064bb132f86e4b7a244801c888d5be
Vivobook 16X <edmonqocharyan0@gmail.com>
2025-01-18 21:31:20 +0400

    My Big Project

--- Big_Project/app/src/main/java/com/example/big_project/Info1Activity.java
+++ Big_Project/app/src/main/java/com/example/big_project/Info1Activity.java
@@ -1,15 +1,24 @@
 package com.example.big_project;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class Info1Activity extends AppCompatActivity {
+
+    TextView info;
+
+    TextView aftor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info1);
 
+        info = findViewById(R.id.Text_What);
+        aftor = findViewById(R.id.Text_What1);
     }
 }
